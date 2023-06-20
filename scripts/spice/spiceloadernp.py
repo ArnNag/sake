33c2b8699854d26016c25d9b9726c94b0172df00
Arnav <anagle@pop-os.localdomain>
2023-06-20 14:41:04 -0400

    Switch from list extend to np.concatenate

--- scripts/spice/spiceloadernp.py
+++ scripts/spice/spiceloadernp.py
@@ -40,15 +40,15 @@ class SPICESerializer:
             pos_arr = self.data[name]['conformations']
             forces_arr = self.data[name]['dft_total_gradient']
             energy_arr = self.data[name]['dft_total_energy']
-            all_energies.extend(energy_arr)
             pad_num = self.max_atom_num - len(atom_nums)
             padded_atom_nums = np.pad(atom_nums, (0, pad_num))
             padded_pos = np.pad(pos_arr, ((0, 0), (0, pad_num), (0, 0)))
             padded_forces = np.pad(forces_arr, ((0, 0), (0, pad_num), (0, 0)))
-            all_atom_nums.extend([padded_atom_nums for conf in range(len(pos_arr))])
-            all_forces.extend(padded_forces)
-            all_pos.extend(padded_pos)
-        np.savez(out_path, atomic_numbers=np.array(all_atom_nums), total_energy=np.array(all_energies), forces=np.array(all_forces), pos=np.array(all_pos))
+            all_atom_nums.append([padded_atom_nums for conf in range(len(pos_arr))])
+            all_energies.append(energy_arr)
+            all_forces.append(padded_forces)
+            all_pos.append(padded_pos)
+        np.savez(out_path, atomic_numbers=np.concatenate(all_atom_nums), total_energy=np.concatenate(all_energies), forces=np.concatenate(all_forces), pos=np.concatenate(all_pos))
 
 
 class SpiceLoader:
