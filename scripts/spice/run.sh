058b0938302d40febc4311369b0c8d00df886cb5
Arnav Nagle <naglea@lt06.hpc.private>
2023-06-22 10:54:46 -0400

    Try to make run.sh for run_batch_force_loss.py

--- scripts/spice/run.sh
+++ scripts/spice/run.sh
@@ -1,6 +1,3 @@
-for name in U0 # alpha homo lumo gap r2 omega1 zpve U0 U H G Cv
-do
-    # bsub -q gpuqueue -o %J.stdout -gpu "num=1:j_exclusive=yes" -R "rusage[mem=5] span[ptile=1]" -R V100 -W 119:59 -n 1\
-    bsub -q cpuqueue -o %J.stdout -R "rusage[mem=1] span[ptile=30]" -W 8:59 -n 30 \
-    python run.py $name
-done
+eval "$(conda shell.bash hook)"
+conda activate sake
+bsub -q gpuqueue -o %J.stdout -R "rusage[mem=20] span[ptile=1]" -W 00:01 -n 1 python run_batch_force_loss.py
