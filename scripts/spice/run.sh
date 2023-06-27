d5c9e7b2cb4963ea64c1732e1aa3e158f9b938a2
Arnav <anagle@pop-os.localdomain>
2023-06-27 17:33:23 -0400

    Update run.sh to use node with enough GPU memory

--- scripts/spice/run.sh
+++ scripts/spice/run.sh
@@ -1,3 +1,5 @@
 eval "$(conda shell.bash hook)"
-conda activate sake
-bsub -u "lt-gpu" -q gpuqueue -gpu "num=1:j_exclusive=yes" -o %J.stdout -R "rusage[mem=100] span[ptile=1]" -W 10:00 -n 1 python run_batch_force_loss.py "small_"
+conda activate jax
+bsub -q gpuqueue -gpu "num=1:j_exclusive=yes" -o %J.stdout -R "rusage[mem=5] span[ptile=1]" -R A40 -W 24:00 -n 1 python run.py "full_60_"
+
+
