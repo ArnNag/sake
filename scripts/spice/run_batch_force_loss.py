a8777eb5937a80437c02ecaac994b5af4bffc832
Arnav <anagle@pop-os.localdomain>
2023-06-27 10:19:15 -0400

    Replace onp with jnp

--- scripts/spice/run_batch_force_loss.py
+++ scripts/spice/run_batch_force_loss.py
@@ -181,7 +181,7 @@ class SPICEBatchLoader:
         f_batch = self.f_tr[batch_idxs]
         _m = i_nums > 0
         m_batch = jnp.einsum("bn,bN->bnN", _m, _m) 
-        y_batch = onp.expand_dims(self.y_tr[batch_idxs], -1)
+        y_batch = jnp.expand_dims(self.y_tr[batch_idxs], -1)
         jax.debug.print("i_nums: {i_nums}, i_batch: {i_batch}, x_batch: {x_batch}, f_batch: {f_batch}, _m: {_m}, m_batch: {m_batch}, y_batch: {y_batch}", i_nums=i_nums.shape, i_batch=i_batch.shape, x_batch=x_batch.shape, f_batch=f_batch.shape, _m=_m.shape, m_batch=m_batch.shape, y_batch=y_batch.shape)
         return i_batch, x_batch, m_batch, f_batch, y_batch  
 
