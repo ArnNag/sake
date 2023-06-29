3cf7dc9a02ab6ff9533947c0d92b32a02f6b3877
Arnav <anagle@pop-os.localdomain>
2023-06-29 10:39:43 -0400

    Adapt encoding for SPICE

--- scripts/spice/eval.py
+++ scripts/spice/eval.py
@@ -6,20 +6,22 @@ import numpy as onp
 import sake
 import tqdm
 
-def run(target):
+def run(prefix):
  
-    ds_tr, ds_vl, ds_te = onp.load("train.npz"), onp.load("valid.npz"), onp.load("test.npz")
-    i_tr, i_vl, i_te = ds_tr["charges"], ds_vl["charges"], ds_te["charges"]
-    x_tr, x_vl, x_te = ds_tr["positions"], ds_vl["positions"], ds_te["positions"]
-    y_tr, y_vl, y_te = ds_tr[target], ds_vl[target], ds_te[target]
-    
-    if target + "_thermo" in ds_tr:
-        y_tr = y_tr - ds_tr[target + "_thermo"]
-        y_vl = y_vl - ds_vl[target + "_thermo"]
-        y_te = y_te - ds_te[target + "_thermo"]
+    ds_tr, ds_vl = onp.load(prefix + "spice_train.npz"), onp.load(prefix + "spice_valid.npz")
 
-    y_tr, y_vl, y_te = onp.expand_dims(y_tr, -1), onp.expand_dims(y_vl, -1), onp.expand_dims(y_te, -1)
-    m_tr, m_vl, m_te = (i_tr > 0), (i_vl > 0), (i_te > 0)
+    i_tr, i_vl = ds_tr["atomic_numbers"], ds_vl["atomic_numbers"]
+    # Index into ELEMENT_MAP is atomic number, value is type number. -99 indicates element not in dataset.
+    ELEMENT_MAP = onp.array([ 0,  1, -99,  2, -99, -99,  3,  4,  5,  6, -99,  7,  8, -99, -99,  9, 10, 11, -99, 12, 13, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, 14, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, 15]) 
+    NUM_ELEMENTS = 16
+    i_tr, i_vl = ELEMENT_MAP[i_tr], ELEMENT_MAP[i_vl]
+
+    x_tr, x_vl = ds_tr["pos"], ds_vl["pos"]
+    f_tr, f_vl = ds_tr["forces"], ds_vl["forces"]
+    y_tr, y_vl = ds_tr["total_energy"], ds_vl["total_energy"]
+    
+    y_tr, y_vl = onp.expand_dims(y_tr, -1), onp.expand_dims(y_vl, -1)
+    m_tr, m_vl = (i_tr > 0), (i_vl > 0)
 
     def make_edge_mask(m):
         return jnp.expand_dims(m, -1) * jnp.expand_dims(m, -2)
@@ -27,16 +29,13 @@ def run(target):
     def sum_mask(m):
         return jnp.sign(m.sum(-1, keepdims=True))
 
-    for _var in ["i", "x", "y", "m"]:
-        for _split in ["tr", "vl", "te"]:
+    for _var in ["i", "x", "y", "m", "f"]:
+        for _split in ["tr", "vl"]:
             locals()["%s_%s" % (_var, _split)] = jnp.array(locals()["%s_%s" % (_var, _split)])
 
 
-    i_tr, i_vl, i_te = jax.nn.one_hot(i_tr, i_tr.max()+1), jax.nn.one_hot(i_vl, i_tr.max()+1), jax.nn.one_hot(i_te, i_tr.max()+1)
-    m_tr, m_vl, m_te = make_edge_mask(m_tr), make_edge_mask(m_vl), make_edge_mask(m_te)
-
-    BATCH_SIZE = 128
-    N_BATCHES = len(i_tr) // BATCH_SIZE
+    i_tr, i_vl = jax.nn.one_hot(NUM_ELEMENTS), jax.nn.one_hot(NUM_ELEMENTS)
+    m_tr, m_vl = make_edge_mask(m_tr), make_edge_mask(m_vl)
 
     from sake.utils import coloring
     from functools import partial
@@ -77,7 +76,7 @@ def run(target):
         return y_hat
 
     from flax.training.checkpoints import restore_checkpoint
-    state = restore_checkpoint("_" + target, None)
+    state = restore_checkpoint("_" + prefix, None)
     params = state['params']
 
     def _get_y_hat(inputs):
@@ -91,14 +90,10 @@ def run(target):
     inputs = jnp.concatenate([x_vl, i_vl], axis=-1)
     y_vl_hat = jax.lax.map(_get_y_hat, inputs)
 
-    inputs = jnp.concatenate([x_te, i_te], axis=-1)
-    y_te_hat = jax.lax.map(_get_y_hat, inputs)
-
     print(y_tr_hat)
     
     print("training", sake.utils.bootstrap_mae(y_tr_hat, y_tr))
     print("validation", sake.utils.bootstrap_mae(y_vl_hat, y_vl))
-    print("test", sake.utils.bootstrap_mae(y_te_hat, y_te))
 
 if __name__ == "__main__":
     import sys
