12d02237f3c54cc41685beb98599663ce26ec46f
Arnav Nagle <naglea@lilac-ln02.hpc.private>
2023-07-03 18:27:51 -0400

    Running up to 100 epochs with batch size 56

--- scripts/spice/run.sh
+++ scripts/spice/run.sh
@@ -1,5 +1,5 @@
 eval "$(conda shell.bash hook)"
 conda activate jax
-bsub -q gpuqueue -gpu "num=1:j_exclusive=yes" -o %J.stdout -R "rusage[mem=5] span[ptile=1]" -R A40 -W 24:00 -n 1 python run.py "full_60_"
+bsub -q gpuqueue -gpu "num=1:j_exclusive=yes" -o %J.stdout -R "rusage[mem=10] span[ptile=1]" -R A40 -W 72:00 -n 1 python run.py "full_60_"
 
 
