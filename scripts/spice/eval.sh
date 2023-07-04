0a7c25774f041e6aa618898ceaba2f50af03c6b1
Arnav Nagle <naglea@lilac-ln02.hpc.private>
2023-07-03 23:36:43 -0400

    Fix naming for eval

--- scripts/spice/eval.sh
+++ scripts/spice/eval.sh
@@ -1,6 +1,3 @@
-for name in mu alpha homo lumo gap r2 omega1 zpve U0 U H G Cv
-
-do
-    bsub -q gpuqueue -o %J.stdout -gpu "num=1:j_exclusive=yes" -R "rusage[mem=5] span[ptile=1]" -W 7:59 -n 1\
-    python eval.py $name
-done
+eval "$(conda shell.bash hook)"
+conda activate jax
+bsub -q gpuqueue -o %J.stdout -gpu "num=1:j_exclusive=yes" -R "rusage[mem=50] span[ptile=1]" -W 3:59 -n 1 -R A40 python eval.py full_60_
