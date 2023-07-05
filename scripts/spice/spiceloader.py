7db16c5a5038a1f7a1c74d95bf660ec5457419c7
Arnav Nagle <rnow03@gmail.com>
2023-07-05 10:54:25 -0400

    Fix force with negative sign on gradient. Use formation energy instead of total energy

--- scripts/spice/spiceloader.py
+++ scripts/spice/spiceloader.py
@@ -1,74 +1,58 @@
 import h5py
 import jax
-import jax.numpy as jnp
+import numpy as np
 import time
-import os
 import json
+import os
+import sys
 
-class SPICELoader:
+class SPICESerializer:
 
-    def __init__(self, file_path, train_ratio, test_ratio, batch_size=128, seed=2666, max_atom_num=96):
-        self.data = h5py.File(file_path)
+    def __init__(self, data_path, out_prefix, train_ratio, test_ratio, seed=2666, max_atom_num=96):
+        self.data = h5py.File(data_path, 'r')
         self.names = list(self.data.keys())
-        self.key = jax.random.PRNGKey(seed)
-        self.batch_size = batch_size
+        key = jax.random.PRNGKey(seed)
         self.max_atom_num = max_atom_num
+        self.test_names, self.train_names, self.val_names = self._split(key, train_ratio, test_ratio)
+        self._make_npz(self.train_names, out_prefix + "spice_train")
+        self._make_npz(self.test_names, out_prefix + "spice_test")
+        self._make_npz(self.val_names, out_prefix + "spice_val")
+
+    def _split(self, key, train_ratio, test_ratio):
         n_samples = len(self.names)
-        split_idxs = jax.random.shuffle(self.key, jnp.arange(n_samples))
+        split_idxs = jax.random.permutation(key, n_samples)
         n_test = int(test_ratio * n_samples)
         n_train = int(train_ratio * n_samples)
-        self.test_names = [self.names[i] for i in split_idxs[:n_test]]
-        self.train_names = [self.names[i] for i in split_idxs[n_test:n_test + n_train]]
-        self.val_names = [self.names[i] for i in split_idxs[n_test + n_train:]]
-        if not os.path.isfile('conf_idxs.json'):
-            self._make_conf_idxs(self.train_names)
-        with open('conf_idxs.json', 'r') as f:
-            self.conf_idxs = json.load(f)
+        test_names = [self.names[i] for i in split_idxs[:n_test]]
+        train_names = [self.names[i] for i in split_idxs[n_test:n_test + n_train]]
+        val_names = [self.names[i] for i in split_idxs[n_test + n_train:]]
+        return test_names, train_names, val_names
 
-    def _make_conf_idxs(self, train_names):
-        conf_idxs = []
-        for name in train_names:
-            for i in range(len(self.data[name]["conformations"])):
-                conf_idxs.append((name, i))
-        with open('conf_idxs.json', 'w') as f:
-            json.dump(conf_idxs, f)
-
-    def _shuffle_confs(self, conf_idxs):
-        return jax.random.permutation(self.key, len(conf_idxs))
-
-    def _get_batch(self, train_order, batch_num):
-        batch_idxs = train_order[batch_num * self.batch_size:(batch_num + 1) * self.batch_size]
-        batch_confs = []
-        batch_atom_nums = []
-        batch_energies = []
-        for idx in batch_idxs:
-            name, conf_idx = self.conf_idxs[idx]
-            atom_nums = jnp.array(self.data[name]['atomic_numbers'])
-            if len(atom_nums) > self.max_atom_num:  # will lead to nonstatic batch sizes
+    def _make_npz(self, names, out_path):
+        all_pos = []
+        all_atom_nums = []
+        # all_total_energies = []
+        all_form_energies = []
+        all_forces = []
+        for name in names:
+            atom_nums = np.array(self.data[name]['atomic_numbers'], np.uint8)
+            if len(atom_nums) > self.max_atom_num:
+                print("Skipping: ", name)
                 continue
-            padded_conf = jnp.pad(self.data[name]['conformations'][conf_idx], ((0, self.max_atom_num - len(atom_nums)), (0, 0)))
-            batch_confs.append(padded_conf)
-            padded_atom_nums = jnp.pad(atom_nums, (0, self.max_atom_num - len(atom_nums)))
-            batch_atom_nums.append(padded_atom_nums)
-            batch_energies.append(self.data[name]['dft_total_energy'][conf_idx])
-        return jnp.array(batch_confs), jnp.array(batch_atom_nums), jnp.array(batch_energies)
+            pos_arr = self.data[name]['conformations']
+            forces_arr = -self.data[name]['dft_total_gradient']
+            # total_energy_arr = self.data[name]['dft_total_energy']
+            form_energy_arr = self.data[name]['formation_energy']
+            pad_num = self.max_atom_num - len(atom_nums)
+            padded_atom_nums = np.pad(atom_nums, (0, pad_num))
+            padded_pos = np.pad(pos_arr, ((0, 0), (0, pad_num), (0, 0)))
+            padded_forces = np.pad(forces_arr, ((0, 0), (0, pad_num), (0, 0)))
+            all_atom_nums.append([padded_atom_nums for conf in range(len(pos_arr))])
+            # all_total_energies.append(total_energy_arr)
+            all_form_energies.append(form_energy_arr)
+            all_forces.append(padded_forces)
+            all_pos.append(padded_pos)
+        np.savez(out_path, atomic_numbers=np.concatenate(all_atom_nums), formation_energy=np.concatenate(all_form_energies), forces=np.concatenate(all_forces), pos=np.concatenate(all_pos))
 
-    def get_epoch(self):
-        train_order = self._shuffle_confs(self.conf_idxs)
-        for i in range(len(train_order) // self.batch_size):
-            yield self._get_batch(i, train_order)
-
-
-spice_loader = SPICELoader('SPICE-1.1.3.hdf5', train_ratio=0.8, test_ratio=0.1, max_atom_num=60)
-train_order = spice_loader._shuffle_confs(spice_loader.conf_idxs)
-
-print("Starting")
-start = time.time()
-for j in range(100):
-    batch_confs, batch_atom_nums, batch_energies = spice_loader._get_batch(train_order, j)
-    # print(batch_confs.shape)
-    # print(batch_atom_nums.shape)
-    # print(batch_energies.shape)
-
-end = time.time()
-print(end - start)
+if __name__ == "__main__": 
+	spice_serializer = SPICESerializer('SPICE-1.1.3.hdf5', sys.argv[1], train_ratio=float(sys.argv[2]), test_ratio=float(sys.argv[3]), max_atom_num=int(sys.argv[4]))
