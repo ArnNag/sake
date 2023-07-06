37102a50ffc842f3a02031c76cf8bb131054aad8
Arnav Nagle <naglea@lilac-ln02.hpc.private>
2023-07-06 16:34:06 -0400

    Fix subset stuff

--- scripts/spice/eval.sh
+++ scripts/spice/eval.sh
@@ -1,3 +1,3 @@
 eval "$(conda shell.bash hook)"
 conda activate jax
-bsub -q gpuqueue -o %J.stdout -gpu "num=1:j_exclusive=yes" -R "rusage[mem=50] span[ptile=1]" -W 3:59 -n 1 -R A40 python eval.py full_60_
+bsub -q gpuqueue -o %J.stdout -gpu "num=1:j_exclusive=yes" -R "rusage[mem=15] span[ptile=1]" -W 0:10 -n 1 python eval.py _full_60_form_name_batch_56_eloss_1.000000e+00_subset_3 3 3
