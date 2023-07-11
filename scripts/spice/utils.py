2dbf01759c3da78f173a61fdcbe3130f27b6cbb3
Arnav Nagle <naglea@lilac-ln02.hpc.private>
2023-07-11 16:37:30 -0400

    Fix utils.py

--- scripts/spice/utils.py
+++ scripts/spice/utils.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as onp
 
 # Index into ELEMENT_MAP is atomic number, value is type number. -99 indicates element not in dataset.
 ELEMENT_MAP = onp.array([ 0,  1, -99,  2, -99, -99,  3,  4,  5,  6, -99,  7,  8, -99, -99,  9, 10, 11, -99, 12, 13, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, 14, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, -99, 15]) 
@@ -7,7 +7,7 @@ NUM_ELEMENTS = 16
 
 def select(subset_labels, subset, *fields):
     selection = (subset_labels == subset)
-    return (field[selection] for field in *fields)
+    return (field[selection] for field in fields)
 
 """
 Geometry (n_atoms, 3) -> pairwise distances (n_atoms, n_atoms)
@@ -15,7 +15,7 @@ Batched geometry (batch_size, n_atoms, 3) -> pairwise distances (batch_size, n_a
 """
 def distance_matrix(pos):
     assert(len(pos.shape) == 2 or len(pos.shape) == 3)
-    return np.linalg.norm(np.expand_dims(pos, -2) - np.expand_dims(pos, -3), axis=-1)
+    return onp.linalg.norm(onp.expand_dims(pos, -2) - onp.expand_dims(pos, -3), axis=-1)
 
 
 """
@@ -24,7 +24,7 @@ Geometry (n_atoms, 3) -> indices of edges within L (n_edges, 2)
 def radius_graph(pos, L):
     print(pos)
     assert(len(pos.shape) == 2)
-    return np.argwhere((distance_matrix(pos) < L) & ~np.identity(pos.shape[-2], dtype=bool))
+    return onp.argwhere((distance_matrix(pos) < L) & ~onp.identity(pos.shape[-2], dtype=bool))
 
 """
 Batched geometry (batch_size, n_atoms, 3) -> indices of edges within L (batch_size, n_edges, 2)
@@ -38,5 +38,5 @@ def batch_radius_graph(batch_pos, L, max_edges):
         if pad_len < 0:
             print(f"Skipping {i}")
             continue
-        all_edges.append(np.pad(edges, ((0, pad_len), (0, 0)), mode="constant", constant_values=-1))
-    return np.array(all_edges, dtype=np.int8)
+        all_edges.append(onp.pad(edges, ((0, pad_len), (0, 0)), mode="constant", constant_values=-1))
+    return onp.array(all_edges, dtype=onp.int8)
