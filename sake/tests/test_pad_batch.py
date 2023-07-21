6d88b5258371ad124bfe3f99cf5f15ddfabc6747
Arnav Nagle <naglea@lt08.hpc.private>
2023-07-21 18:58:31 -0400

    Try to implement test data for loader. Failing.

--- sake/tests/test_pad_batch.py
+++ sake/tests/test_pad_batch.py
@@ -6,7 +6,8 @@ def test_pad():
     import sake
     real_nodes = 5
     dummy_nodes = 7
-    dummy_edges = 3
+    dummy_edges_one = 3
+    dummy_edges_two = 11
     hidden_features = 2
     max_nodes = real_nodes + dummy_nodes
     model = sake.layers.SparseSAKELayer(hidden_features, hidden_features)
@@ -21,10 +22,66 @@ def test_pad():
     h_pad_one = jnp.concatenate((h, h_dummy_one), axis=0)
     h_pad_two = jnp.concatenate((h, h_dummy_two), axis=0)
     real_edges = jnp.argwhere(jnp.logical_not(jnp.identity(real_nodes)))
-    edges_pad = jnp.pad(real_edges, ((0, dummy_edges), (0, 0)), constant_values=-1)
-    init_params = model.init(jax.random.PRNGKey(2046), h_pad_one, x_pad_one, edges=edges_pad)
-    h_out_one, x_out_one, v_out_one = model.apply(init_params, h_pad_one, x_pad_one, edges=edges_pad)
-    h_out_two, x_out_two, v_out_two = model.apply(init_params, h_pad_two, x_pad_two, edges=edges_pad)
+    edges_pad_one = jnp.pad(real_edges, ((0, dummy_edges_one), (0, 0)), constant_values=-1)
+    edges_pad_two = jnp.pad(real_edges, ((0, dummy_edges_two), (0, 0)), constant_values=-1)
+    init_params = model.init(jax.random.PRNGKey(2046), h_pad_one, x_pad_one, edges=edges_pad_one)
+    h_out_one, x_out_one, v_out_one = model.apply(init_params, h_pad_one, x_pad_one, edges=edges_pad_one)
+    h_out_two, x_out_two, v_out_two = model.apply(init_params, h_pad_two, x_pad_two, edges=edges_pad_two)
     assert jnp.allclose(h_out_one[:real_nodes], h_out_two[:real_nodes])
     assert jnp.allclose(x_out_one[:real_nodes], x_out_two[:real_nodes])
     assert jnp.allclose(v_out_one[:real_nodes], v_out_two[:real_nodes])
+
+def test_batch_pad():
+    import sys
+    import jax
+    import jax.numpy as jnp
+    import sake
+    sys.path.append('../../scripts/spice')
+    from sparse import SPICEBatchLoader
+
+    num_elements = 17
+    x_features = 3
+    f_features = 3
+    y_features = 1
+    num_graphs_tr = 997
+    num_nodes_tr = 7
+    num_edges_tr = 5
+    max_graphs_batch = 29
+    max_nodes_batch = 53
+    max_edges_batch = 59
+    key = jax.random.PRNGKey(2666)
+    nodes_per_graph = jax.random.randint(key, shape=(num_graphs_tr,), minval=2, maxval=num_nodes_tr)
+    edges_per_graph = jax.random.randint(key, shape=(num_graphs_tr,), minval=1, maxval=num_edges_tr)
+    i_tr = []
+    x_tr = []
+    edges_tr = []
+    f_tr = []
+    y_tr = []
+
+    for i, (num_nodes, num_edges) in enumerate(zip(nodes_per_graph, edges_per_graph)):
+        key = jax.random.PRNGKey(i)
+        i = jnp.pad(jax.random.randint(key, shape=(num_nodes,), minval=0, maxval=num_elements-1), ((0, num_nodes_tr - num_nodes)))
+        x = jnp.pad(jax.random.uniform(key, shape=(num_nodes, x_features)), ((0, num_nodes_tr - num_nodes), (0, 0)))
+        all_edges = jnp.argwhere(jnp.logical_not(jnp.identity(num_nodes))) 
+        edges = jnp.pad(all_edges[jax.random.permutation(key, num_edges_tr)[:num_edges]], ((0, num_edges_tr - num_edges), (0, 0)), mode='constant', constant_values=-1)
+        f = jnp.pad(jax.random.uniform(key, shape=(num_nodes, f_features)), ((0, num_nodes_tr - num_nodes), (0, 0)))
+        y = jnp.pad(jax.random.uniform(key, shape=(num_nodes, y_features)), ((0, num_nodes_tr - num_nodes), (0, 0)))
+        i_tr.append(i)
+        x_tr.append(x)
+        edges_tr.append(edges)
+        f_tr.append(f)
+        y_tr.append(y)
+    i_tr = jnp.array(i_tr)
+    x_tr = jnp.array(x_tr)
+    edges_tr = jnp.array(edges_tr)
+    f_tr = jnp.array(f_tr)
+    y_tr = jnp.array(y_tr)
+    assert(i_tr.shape == (num_graphs_tr, num_nodes_tr))
+    assert(x_tr.shape == (num_graphs_tr, num_nodes_tr, x_features))
+    assert(edges_tr.shape == (num_graphs_tr, num_edges_tr, 2))
+    assert(f_tr.shape == (num_graphs_tr, num_nodes_tr, f_features))
+    assert(y_tr.shape == (num_graphs_tr, num_nodes_tr, y_features))
+            
+    print("nodes_per_graph", nodes_per_graph)
+    print("edges_per_graph", edges_per_graph)
+    loader = SPICEBatchLoader(i_tr, x_tr, edges_tr, f_tr, y_tr, nodes_per_graph, edges_per_graph, 1776, max_edges_batch, max_nodes_batch, max_graphs_batch, num_elements)
