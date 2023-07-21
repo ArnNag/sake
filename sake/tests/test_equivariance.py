c6c7e0c1b9a7c0109196903339139c5f537d56ac
Arnav Nagle <naglea@lt15.hpc.private>
2023-07-21 11:13:18 -0400

    Passes equivarence test

--- sake/tests/test_equivariance.py
+++ sake/tests/test_equivariance.py
@@ -43,3 +43,28 @@ def test_model_equivariance(_equivariance_test_utils):
     assert jnp.allclose(x_translation, translation(x_original))
     assert jnp.allclose(x_rotation, rotation(x_original))
     assert jnp.allclose(x_reflection, reflection(x_original))
+
+def test_sparse_layer_equivariance(_equivariance_test_utils):
+    import jax
+    import jax.numpy as jnp
+    import sake
+    h0, x0, v0, translation, rotation, reflection = _equivariance_test_utils
+    max_nodes = 5
+    model = sake.layers.SparseSAKELayer(7, 7)
+    edges = jnp.argwhere(jnp.logical_not(jnp.identity(max_nodes)))
+
+    init_params = model.init(jax.random.PRNGKey(2666), h0, x0, v0, edges=edges, max_nodes=max_nodes)
+
+    h_original, x_original, v_original = model.apply(init_params, h0, x0, v0, edges=edges, max_nodes=max_nodes)
+    h_translation, x_translation, v_translation = model.apply(init_params, h0, translation(x0), v0, edges=edges, max_nodes=max_nodes)
+    h_rotation, x_rotation, v_rotation = model.apply(init_params, h0, rotation(x0), rotation(v0), edges=edges, max_nodes=max_nodes)
+    h_reflection, x_reflection, v_reflection = model.apply(init_params, h0, reflection(x0), reflection(v0), edges=edges, max_nodes=max_nodes)
+
+    assert jnp.allclose(h_translation, h_original)
+    assert jnp.allclose(h_rotation, h_original)
+    assert jnp.allclose(h_reflection, h_original)
+
+    assert jnp.allclose(x_translation, translation(x_original))
+    assert jnp.allclose(x_rotation, rotation(x_original))
+    assert jnp.allclose(x_reflection, reflection(x_original))
+
