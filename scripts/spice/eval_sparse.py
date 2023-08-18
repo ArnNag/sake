be8bc71b3773cdfbff0c4e3873b08cf1b7978c7f
Arnav Nagle <naglea@lilac-ln02.hpc.private>
2023-08-18 13:31:16 -0400

    Reimplement eval_sparse for Jraph

--- scripts/spice/eval_sparse.py
+++ scripts/spice/eval_sparse.py
@@ -8,24 +8,23 @@ from functools import partial
 import tqdm
 import os
 import time
-from utils import load_data, NUM_ELEMENTS, SPICEBatchLoader, SparseSAKEEnergyModel, get_f_loss, get_y_loss
+from utils import load_data, NUM_ELEMENTS, make_batch_loader, SAKEEnergyModel, get_f_loss, get_y_loss
 
 
 def run(param_path, val_path, max_nodes=7200, max_edges=120000, max_graphs=1000, train_subset=-1, val_subset=3, seed=0):
-    i_vl, x_vl, edges_vl, f_vl, y_vl, num_nodes_vl, num_edges_vl = load_data(val_path, val_subset)
+    graph_list = load_data(val_path, val_subset)[0]
     print("loaded")
 
-    model = SparseSAKEEnergyModel(num_segments=max_graphs)
+    model = SAKEEnergyModel()
 
-    loader = SPICEBatchLoader(i_vl, x_vl, edges_vl, f_vl, y_vl, num_nodes_vl, num_edges_vl, seed, max_edges, max_nodes, max_graphs, NUM_ELEMENTS)
+    batch_loader = make_batch_loader(graph_list, seed=seed, max_nodes=max_nodes, max_edges=max_edges, max_graphs=max_graphs, num_elements=NUM_ELEMENTS)
 
     def predict(params):
         total_f_loss = 0
         total_y_loss = 0
-        for idx in range(len(loader)):
-            i, x, edges, f, y, graph_segments = loader.get_batch(idx)  
-            total_f_loss += get_f_loss(model, params, i, x, edges, f, graph_segments)
-            total_y_loss += get_y_loss(model, params, i, x, edges, y, graph_segments)
+        for graph in batch_loader:
+            total_f_loss += get_f_loss(model, params, graph)
+            total_y_loss += get_y_loss(model, params, graph)
         return total_f_loss, total_y_loss
 
     from flax.training.checkpoints import restore_checkpoint
